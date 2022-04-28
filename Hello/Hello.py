b8e9d8a53de3f139a27ce0543a144ab3dfabdcaa
Christoffer Krull <christoffer.krull.mte22lin@edu.tucsweden.se>
2022-04-28 09:01:07 +0200

    Klar med uppgift 1

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -3,3 +3,19 @@ var  = "Hello world"
 print (var)
 print ("Hej Chris")
 print ("Hej Krull")
+
+
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return "More organized code", "More readable code", "Easier code reuse","Allowing programmers to share and connect code together"
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return f"{benefit} is a benefit of funtion"
+
+def name_the_benefits_of_functions():
+
+    for benefit in list_benefits():
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
---
+++ Hello/Uppgift 1.py
@@ -0,0 +1,12 @@
+## Create a function add, to calculate a the sum of two numbers, add(x, y)
+# Create a second function test_add which tests that the add function using assert
+# For instance assert add(1, 2) == 3
+
+def theproduktoftwonumbers(a,b):
+    return a*b
+print(theproduktoftwonumbers(6,7))
+
+
+assert theproduktoftwonumbers(6,7) == 42
+print("Du är King")
+
