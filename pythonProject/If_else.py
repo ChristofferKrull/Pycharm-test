6d0508001482ebdcc3f47867ef38828b47cda4e2
Christoffer Krull <christoffer.krull.mte22lin@edu.tucsweden.se>
2022-05-17 16:36:24 +0200

    Tagit bort

---
+++ pythonProject/If_else.py
@@ -0,0 +1,11 @@
+a = 40
+b = 4
+
+if a < b:
+    print("Du äger på programering")
+elif a == b:
+    print("du är king")
+elif a > b + 10:
+    print("fyfan va du är kylig")
+else:
+    print("Fyfan du är sån kuuung")
